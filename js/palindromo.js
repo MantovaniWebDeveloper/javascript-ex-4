359ae03444dbe15b2b00fb3ef08816a6022bd4d2
MantovaniWebDeveloper <ratedsuperstar84@gmail.com>
2018-11-19 14:13:52 +0100

    logica scritta in italiano fatta

---
+++ js/palindromo.js
@@ -0,0 +1,15 @@
+//chiedo all'utente una parola
+var parolaUtente = prompt("Inserisci la parola");
+
+console.log("Stringa ricevuta dall'utente: " +parolaUtente)
+
+//creo una funzione per controllare la parola
+function controlloPalindromo(parolaUtente) {
+  //setto una variabile in modalita false da cambiare in caso di
+  //parola palindroma
+  //faccio un confronto tra la versione della parola normale
+  // e la stessa parola letta ne senso opposto
+  //ritorno il risultato
+}
+ //richiamo la funzione in variabile
+ //la stampo su console
