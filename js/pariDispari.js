6ba449dd3193bde14378964748abf4bafaa253e3
MantovaniWebDeveloper <ratedsuperstar84@gmail.com>
2018-11-19 16:10:28 +0100

    inserito cotrollo vittoria

--- js/pariDispari.js
+++ js/pariDispari.js
@@ -1,5 +1,6 @@
 //chiedo all'utente un numero da 1 a 5
-var numeroUtente = prompt(parseInt("inserisca un numero da 1 a 5"));
+var numeroUtente = parseInt(prompt("inserisca un numero da 1 a 5"));
+console.log(numeroUtente);
 
 //generiamo numero random per il pc da 1 a 5
 //passando tra gli argomenti 2 valori
@@ -11,3 +12,13 @@ function numeroRandom(numeroMin, numeroMax){
 //lancio la funzione per la cpu
 var randomCpu = numeroRandom(1,6);
 console.log(randomCpu);
+
+if (numeroUtente > randomCpu) {
+  console.log("Utente hai vitnto");
+}
+else if (numeroUtente == randomCpu) {
+  console.log("pareggio");
+}
+else {
+  console.log("CPU VINCE");
+}
