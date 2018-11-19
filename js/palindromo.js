4572c6b9fcbd7273c9de9762a0e9656ccf070c11
MantovaniWebDeveloper <ratedsuperstar84@gmail.com>
2018-11-19 14:25:59 +0100

    prova a ciclare sulla parola e invertire il senso

--- js/palindromo.js
+++ js/palindromo.js
@@ -1,15 +1,22 @@
 //chiedo all'utente una parola
 var parolaUtente = prompt("Inserisci la parola");
 
-console.log("Stringa ricevuta dall'utente: " +parolaUtente)
+console.log("Stringa ricevuta dall'utente: " + parolaUtente)
 
 //creo una funzione per controllare la parola
 function controlloPalindromo(parolaUtente) {
   //setto una variabile in modalita false da cambiare in caso di
   //parola palindroma
+  var palindroma = false;
+  var arrayParola = [];
+  for (var i = 0; i < parolaUtente.length ; i++) {
+    console.log(parolaUtente[i]);
+  }
   //faccio un confronto tra la versione della parola normale
   // e la stessa parola letta ne senso opposto
   //ritorno il risultato
 }
  //richiamo la funzione in variabile
+ var risultato = controlloPalindromo(parolaUtente);
  //la stampo su console
+console.log(risultato);
